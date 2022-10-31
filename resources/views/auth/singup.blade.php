0ed292a18dec9f863c0e6805a4be8934423a3c21
Azran Hossain <mr69751@gmail.com>
2022-10-31 13:15:16 +0600

    base template

---
+++ resources/views/auth/singup.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@include('layouts.nav')
+@section('content')
+    <div class="flex justify-center items-center h-screen">
+
+        <div class="flex flex-col bg-gray-900 p-20 rounded md:px-8 py-10">
+            <h1 class=" text-2xl font-bold text-gray-200 text-center mb-8 md:text-3xl md:mb-10">Sign in to your account</h1>
+            <p>Your Email:</p>
+            <input class="bg-gray-800 my-2 rounded p-3 md:pr-40" type="text" name="email" id=""
+                placeholder="name@email.com">
+            <p>Password:</p>
+            <input class="bg-gray-800 my-2 rounded p-3 md:pr-40" type="password" name="password" id=""
+                placeholder="**********">
+            <div class="flex justify-between mt-3">
+
+                <div class="flex items-center">
+                    <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4">
+                    <label for="link-checkbox" class="ml-2 font-medium text-gray-900 dark:text-gray-300">Remember
+                        me</label>
+                </div>
+
+                <a class="text-blue-500" href="#">Forgot Password?</a>
+            </div>
+
+            <button class="my-2 rounded-xl p-2 mt-5 md:px-32 bg-blue-600">Sign in</button>
+            <a class="text-center mt-5 text-blue-500" href="/register">Dont't have an account?</a>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+{{-- nav bar --}}
+<div class="bg-gray-900 shadow shadow-gray-700">
+    <div class="flex justify-between lg:justify-around items-center">
+        <div class="font-Bungee text-xl lg:text-3xl px-4"><a href="/">Farmer's Assistant</a></div>
+        <nav class="flex gap-8 p-3 justify-center">
+            <div><a class="flex items-center gap-2 hover:text-orange-500" href="/signup">
+                    @svg('iconsax-bro-signpost', 'w-5') Register</a></div>
+            <div class="pr-4"><a class="flex items-center gap-2 hover:text-orange-500" href="/signin">
+                    @svg('tni-signin-o', 'w-5') sign in</a></div>
+        </nav>
+    </div>
+</div>
