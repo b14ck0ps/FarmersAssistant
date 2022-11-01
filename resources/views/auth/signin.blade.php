29629e956c32537564715bf758d6fe48d3e2a94c
Azran Hossain <mr69751@gmail.com>
2022-11-01 18:46:54 +0600

    error msz added

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -1,26 +1,34 @@
 @extends('layouts.app')
 @section('content')
     <div class="flex justify-center items-center h-screen">
+        <form action="login" method="POST">
+            @csrf
+            <div class="flex flex-col bg-gray-900 rounded md:px-8 py-10">
+                <h1 class=" text-2xl font-bold text-gray-200 text-center mb-8 md:text-3xl md:mb-10">Sign in to your account
+                </h1>
+                {{-- error --}}
+                @if ($errors->any())
+                    <h4 class="text-red-700 text-center p-3">{{ $errors->first() }}</h4>
+                @endif
+                @csrf
+                <p>Your Email:</p>
+                <x-forms.input type="email" name="email" placeholder="name@mail.com" />
+                <p>Password:</p>
+                <x-forms.input type="password" name="password" placeholder="********" />
+                <div class="flex justify-between mt-3">
 
-        <div class="flex flex-col bg-gray-900 p-20 rounded md:px-8 py-10">
-            <h1 class=" text-2xl font-bold text-gray-200 text-center mb-8 md:text-3xl md:mb-10">Sign in to your account</h1>
-            <p>Your Email:</p>
-            <x-forms.input type="email" name="email" placeholder="name@mail.com" />
-            <p>Password:</p>
-            <x-forms.input type="password" name="password" placeholder="********" />
-            <div class="flex justify-between mt-3">
+                    <div class="flex items-center">
+                        <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4">
+                        <label for="link-checkbox" class="ml-2 font-medium text-gray-900 dark:text-gray-300">Remember
+                            me</label>
+                    </div>
 
-                <div class="flex items-center">
-                    <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4">
-                    <label for="link-checkbox" class="ml-2 font-medium text-gray-900 dark:text-gray-300">Remember
-                        me</label>
+                    <a class="text-blue-500" href="#">Forgot Password?</a>
                 </div>
 
-                <a class="text-blue-500" href="#">Forgot Password?</a>
-            </div>
-
-            <button class="my-2 rounded-xl p-2 mt-5 md:px-32 bg-blue-600">Sign in</button>
-            <a class="text-center mt-5 text-blue-500" href="/signup">Dont't have an account?</a>
-        </div>
+                <button class="my-2 rounded-xl p-2 mt-5 md:px-32 bg-blue-600">Sign in</button>
+        </form>
+        <a class="text-center mt-5 text-blue-500" href="/signup">Dont't have an account?</a>
+    </div>
     </div>
 @endsection
