06fa35f4b8f837b531386ce63279ff7e62174af3
Azran Hossain <mr69751@gmail.com>
2022-11-01 21:17:33 +0600

    setting linked added to nav bar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 {{-- nav bar --}}
 <div class="bg-gray-900 shadow shadow-gray-700">
-    <div class="flex justify-between lg:justify-around items-center">
+    <div class="flex md justify-between lg:justify-around items-center">
         <div class="font-Bungee text-xl lg:text-3xl px-4"><a href="/">Farmer's Assistant</a></div>
 
         <nav class="flex gap-8 p-3 justify-center">
@@ -11,6 +11,8 @@
                         @svg('tni-signin-o', 'w-5') sign in</a></div>
             @endguest
             @auth
+                <div><a class="flex items-center gap-2 hover:text-orange-500" href="/setting">
+                        @svg('uiw-setting-o', 'w-5') Setting</a></div>
                 <div class="pr-4"><a class="flex items-center gap-2 hover:text-orange-500" href="/logout">
                         @svg('tni-signin-o', 'w-5') Log out</a></div>
             @endauth
