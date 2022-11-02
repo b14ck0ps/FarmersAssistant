389c729640e0fea5b5da726d324cf9936d1c531f
Azran Hossain <mr69751@gmail.com>
2022-11-02 17:24:29 +0600

    button name changed

--- resources/views/auth/singup.blade.php
+++ resources/views/auth/singup.blade.php
@@ -134,7 +134,7 @@
                         conditions</a>.</label>
             </div>
             <button type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Register</button>
         </form>
     </div>
 @endsection
