1cf70f6190c42f0f401bd584b767f84426f60ac3
Azran Hossain <mr69751@gmail.com>
2022-11-06 22:34:46 +0600

    redirect link fixed

--- app/Http/Controllers/Auth/Admins/productsCreateController.php
+++ app/Http/Controllers/Auth/Admins/productsCreateController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth\Admins;
+
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Users\Admins;
@@ -14,7 +15,7 @@ class productsCreateController extends Controller
     public function productcreate(Request $request)
     {
 
-            return view("productshow");
+        return view("productshow");
     }
     public function productsubmit(Request $request)
     {
@@ -24,19 +25,18 @@ class productsCreateController extends Controller
         $usetable->admin_id = session('user_id');
         $usetable->description = $request->description;
 
-        if($request ->hasFile('image'))
-        {
-            $image=$request->file('image');
-            $name =$image->getClientOriginalName();
-            $filename=$name;
-            $image->move('uploads/product',$filename);
-            $usetable->image=$filename;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $name = $image->getClientOriginalName();
+            $filename = $name;
+            $image->move('uploads/product', $filename);
+            $usetable->image = $filename;
         }
 
         $usetable->quantity = $request->quantity;
         $usetable->price = $request->price;
         $usetable->save();
 
-         echo ".......Product create successfully......";
+        return redirect('product_create');
     }
 }
