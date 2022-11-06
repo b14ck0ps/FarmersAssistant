50031e8b5780989a6b0a8cf2e826dc5eec611d9b
Azran Hossain <mr69751@gmail.com>
2022-11-06 23:17:49 +0600

    new migration and models for orders

---
+++ database/migrations/2022_11_06_171201_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total', 8, 2);
+            $table->foreignId('farmer_id')->constrained('farmers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
