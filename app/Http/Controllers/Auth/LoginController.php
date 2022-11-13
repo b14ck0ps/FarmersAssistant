46dcaa661508f1299d86354ac4de025f575671ce
Azran Hossain <mr69751@gmail.com>
2022-11-13 12:15:38 +0600

    fix: advisor login bug

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,10 +35,9 @@ class LoginController extends Controller
                 session(['subs' => User::find(Auth::id())->farmers->first()->subscription]);
                 return redirect()->intended(route('farmers.dashboard'));
             } else if (Auth::user()->getUserType() == 'ADVISOR') {
-                session(['user_id' => User::find(Auth::id())->advisors->first()->id]);
-                dd("advisor dashboard");
-                //$request->session()->regenerate();
-                //return redirect()->intended(route('advisors.dashboard'));
+                session(['user_id' => User::find(Auth::id())->advisor->first()->id]);
+                $request->session()->regenerate();
+                return redirect()->intended(route('advisor.dashboard'));
             } else {
                 session(['user_id' => User::find(Auth::id())->admins->first()->id]);
                 $request->session()->regenerate();
